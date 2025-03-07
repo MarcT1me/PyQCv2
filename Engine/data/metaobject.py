7ffaf6cc0a4c4bbceb4d3da9d3bcee1d7c5a3c40
MarcT1me <marctimeproger@gmail.com>
2025-03-08 01:36:41 +0700

    0.11.0+1  // MetaObjet remove sub-class

--- Engine/data/metaobject.py
+++ Engine/data/metaobject.py
@@ -12,15 +12,6 @@ class MetaObject:
     # MetaData
     id: 'Engine.data.Identifier'
 
-    def __init_subclass__(cls, **kwargs):
-        super().__init_subclass__()
-
-        for method_name in ("event", "events", "pre_update", "update", "pre_render", "render"):
-            if hasattr(cls, method_name):
-                method = getattr(cls, method_name)
-                if isinstance(method, Engine.Callable) and not hasattr(method, "__is_storage__"):
-                    setattr(cls, method_name, Engine.decorators.classed_function(method))
-
     def __init__(self, data: 'Engine.data.MetaData'):
         self.__dict__["_data"] = data
 
